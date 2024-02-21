d48615c0092374ea247d09af4f8f710825b6a187
shaurya singh <shauryasingh96@gmail.com>
2024-02-22 03:11:58 +0530

    Time: 37 ms (61.49%) | Memory: 16.7 MB (87.87%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,26 +1,12 @@
 class Solution:
-    def powerset(self, nums, i, subset, ans):
-        # print(nums, i, subset, ans)
+    def powerset(self, nums, i, subset):
         if i >= len(nums):
-            ans.append(subset)
+            self.ans.append(subset)
             return
-
-        # self.powerset(nums, i+1, subset, ans)
-        # subset.append(nums[i])
-        # self.powerset(nums, i+1, subset, ans)
-
-        self.powerset(nums, i+1, subset, ans)
-        self.powerset(nums, i+1, subset + [nums[i]], ans)
-
-        # self.powerset(nums, i+1, subset + [nums[i]], ans)
-        # self.powerset(nums, i+1, subset, ans)
-
-        # subset.append(nums[i])
-        # self.powerset(nums, i+1, subset, ans)
-        # subset.pop(-1)
-        # self.powerset(nums, i+1, subset, ans)
+        self.powerset(nums, i+1, subset)
+        self.powerset(nums, i+1, subset + [nums[i]])
 
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        self.powerset(nums, 0, [], ans)
-        return ans
+        self.ans = []
+        self.powerset(nums, 0, [])
+        return self.ans
