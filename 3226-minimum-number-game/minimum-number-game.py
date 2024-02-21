c850c69f0a98cf43e1ecd020fe2cf59934492c02
shaurya singh <shauryasingh96@gmail.com>
2024-02-22 02:19:05 +0530

    Time: 56 ms (39.09%) | Memory: 16.6 MB (88.92%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.py
+++ 3226-minimum-number-game/minimum-number-game.py
@@ -2,15 +2,14 @@ class Solution:
     def numberGame(self, nums: List[int]) -> List[int]:
         arr = []
         heap = []
-        for items in nums:
-            heapq.heappush(heap, items)
+        for items in nums: #heapify O(n)
+            heapq.heappush(heap, items) #insertion O(log n)
         
-        while(len(heap) > 0):
-            a = heapq.heappop(heap)
-            b = heapq.heappop(heap)
-            arr.append(b)
-            arr.append(a)
-            print(a, b)
-        print(arr)
+        while(len(heap) > 0): #iteration O(n)
+            a = heapq.heappop(heap) #deletion O(log n)
+            b = heapq.heappop(heap) #deletion O(log n)
+            arr.append(b) #insertion at end O(1)
+            arr.append(a) #insertion at end O(1)
+        # print(arr)
         return arr
         
