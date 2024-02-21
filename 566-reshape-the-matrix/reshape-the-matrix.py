058128dc67e66ee78c2d3075f820d1485e018da3
shaurya singh <shauryasingh96@gmail.com>
2024-02-22 03:06:40 +0530

    Time: 129 ms (12.94%) | Memory: 17.6 MB (51.42%) - LeetSync

--- 566-reshape-the-matrix/reshape-the-matrix.py
+++ 566-reshape-the-matrix/reshape-the-matrix.py
@@ -1,4 +1,5 @@
 class Solution:
+    # approach II - second loop optimized at newMat
     def isValid(self, r1, c1, r, c):
         if r1*c1 == r*c:
             return True
@@ -16,12 +17,34 @@ class Solution:
         print(flatMat)
         newMat = []
         for i in range(r):
-            temp = []
-            for j in range(c):
-                temp.append(flatMat.pop(0))
-                # temp.append(flatMat[i+j])
-            newMat.append(temp)
+            print(newMat)
+            newMat.append(flatMat[i*c: i*c+c])
         return newMat
+        
+# approach I        
+#     def isValid(self, r1, c1, r, c):
+#         if r1*c1 == r*c:
+#             return True
+#         return False
+        
+#     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+#         r1 = len(mat)
+#         c1 = len(mat[0])
+#         if not self.isValid(r1, c1, r, c):
+#             return mat
+#         flatMat = []
+#         for i in range(r1):
+#             for j in range(c1):
+#                 flatMat.append(mat[i][j])
+#         # print(flatMat)
+#         newMat = []
+#         for i in range(r):
+#             temp = []
+#             for j in range(c):
+#                 temp.append(flatMat.pop(0))
+#                 # temp.append(flatMat[i+j])
+#             newMat.append(temp)
+#         return newMat
         
         
         
