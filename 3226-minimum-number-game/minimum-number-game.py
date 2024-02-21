4835f61b3c13efa3064bd6da8066ec25f7b4efd7
shaurya singh <shauryasingh96@gmail.com>
2024-02-22 02:24:56 +0530

    Time: 65 ms (12.01%) | Memory: 16.5 MB (88.92%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.py
+++ 3226-minimum-number-game/minimum-number-game.py
@@ -4,12 +4,12 @@ class Solution:
         heap = []
         for items in nums: #heapify O(n)
             heapq.heappush(heap, items) #insertion O(log n)
-        
+        # overall O(n logn)
         while(len(heap) > 0): #iteration O(n)
             a = heapq.heappop(heap) #deletion O(log n)
             b = heapq.heappop(heap) #deletion O(log n)
             arr.append(b) #insertion at end O(1)
             arr.append(a) #insertion at end O(1)
-        # print(arr)
+        # overall O(n logn)
         return arr
         
