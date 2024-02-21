45b6b36787bfe987d1dabb73f8ca53ece5779351
shaurya singh <shauryasingh96@gmail.com>
2024-02-21 21:23:09 +0530

    Time: 60 ms (94.59%) | Memory: 17.1 MB (80.01%) - LeetSync

--- 1184-car-pooling/car-pooling.py
+++ 1184-car-pooling/car-pooling.py
@@ -3,21 +3,38 @@ class Solution:
         # trips = [[2,1,5],[3,3,7]], capacity = 4
         trips2 = []
         for passenger, st, end in trips:
-            trips2.append([st, passenger, True])
-            trips2.append([end, passenger, False])
-        trips2.sort(key=lambda x: (x[0], x[2]))
+            trips2.append([st, passenger])
+            trips2.append([end, -1 * passenger])
+        trips2.sort() #key=lambda x: (x[0], x[1])
         currPassengers = 0
         # print(trips2)
-        for location, passenger, status in trips2:
-            if status:
-                currPassengers += passenger
-            else:
-                currPassengers -= passenger
+        for location, passenger in trips2:
+            currPassengers += passenger
             # print(currPassengers)
             if currPassengers > capacity or currPassengers < 0:
                 return False
         return True
-        
+
+# approach II to rewrite trips list 
+        # trips2 = []
+        # for passenger, st, end in trips:
+        #     trips2.append([st, passenger, True])
+        #     trips2.append([end, passenger, False])
+        # trips2.sort(key=lambda x: (x[0], x[2]))
+        # currPassengers = 0
+        # # print(trips2)
+        # for location, passenger, status in trips2:
+        #     if status:
+        #         currPassengers += passenger
+        #     else:
+        #         currPassengers -= passenger
+        #     # print(currPassengers)
+        #     if currPassengers > capacity or currPassengers < 0:
+        #         return False
+        # return True
+    
+    
+    
 # approaching problem by keeping two pointers one to track distance and another to track passenger        
 #         i = 0
 #         j = trips[0][]
