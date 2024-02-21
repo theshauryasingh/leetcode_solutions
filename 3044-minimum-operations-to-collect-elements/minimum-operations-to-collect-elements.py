bf4417beda7f52743a3b2e986e62064d7c1448be
shaurya singh <shauryasingh96@gmail.com>
2024-02-21 23:52:23 +0530

    Time: 42 ms (55.68%) | Memory: 16.5 MB (80.45%) - LeetSync

--- 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.py
+++ 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.py
@@ -1,6 +1,6 @@
 class Solution:
-    def minOperations(self, nums: List[int], k: int) -> int:     
-        # approach 1
+    def minOperations(self, nums: List[int], k: int) -> int:
+        # approach 2 - optimizing break
         n = len(nums)
         if k == n:
             return k
@@ -12,7 +12,23 @@ class Solution:
             if nums[i] <= k:
                 coll[nums[i]-1] = True
             if False not in coll:
-                break
+                return counter+1
             counter +=1
             i -=1
-        return counter+1
+        
+# # approach 1
+# n = len(nums)
+# if k == n:
+#     return k
+# counter = 0
+# i = n-1
+# coll = [False]*k
+# while(i > -1):
+#     # print(i, nums[i], coll)
+#     if nums[i] <= k:
+#         coll[nums[i]-1] = True
+#     if False not in coll:
+#         break
+#     counter +=1
+#     i -=1
+# return counter+1
