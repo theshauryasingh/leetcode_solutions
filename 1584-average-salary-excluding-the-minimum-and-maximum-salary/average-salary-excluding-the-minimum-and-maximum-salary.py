b9ad4989e636a904b16865d1c060aff1ed2ffa94
shaurya singh <shauryasingh96@gmail.com>
2024-02-21 23:40:40 +0530

    Time: 39 ms (41.76%) | Memory: 16.6 MB (89.14%) - LeetSync

--- 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.py
+++ 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -1,18 +1,35 @@
 class Solution:
     def average(self, salary: List[int]) -> float:
+        #second approach, optimizing total count
         minSal = float(inf)
         maxSal = float(-inf)
         totalSal = 0
-        count = 0
         for singleSalary in salary:
             if singleSalary < minSal:
                 minSal = singleSalary
             if singleSalary > maxSal:
                 maxSal = singleSalary
             totalSal += singleSalary
-            count +=1
-            # print(minSal, maxSal, totalSal, count)
         totalSal -= minSal
         totalSal -= maxSal
-        return totalSal/(count-2)
+        return totalSal/(len(salary)-2)        
+        
+        
+        
+        #first approach
+        # minSal = float(inf)
+        # maxSal = float(-inf)
+        # totalSal = 0
+        # count = 0
+        # for singleSalary in salary:
+        #     if singleSalary < minSal:
+        #         minSal = singleSalary
+        #     if singleSalary > maxSal:
+        #         maxSal = singleSalary
+        #     totalSal += singleSalary
+        #     count +=1
+        #     # print(minSal, maxSal, totalSal, count)
+        # totalSal -= minSal
+        # totalSal -= maxSal
+        # return totalSal/(count-2)
             
