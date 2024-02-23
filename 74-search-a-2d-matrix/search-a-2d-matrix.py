abd5804c5de3dd10be0b9dc7dd64ef8ca14aafda
shaurya singh <shauryasingh96@gmail.com>
2024-02-24 00:58:14 +0530

    Time: 52 ms (25.21%) | Memory: 17.1 MB (88.45%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,22 +1,46 @@
 class Solution:
-    def binarySearch(self, nums, target):
-        l = 0
-        r = len(nums) - 1
-        while(l<=r):
-            mid = int((l+r) / 2)
-            if nums[mid] == target:
-                return True
-            elif nums[mid] > target:
-                r = mid - 1
-            else:
-                l = mid + 1
-        return False
-    
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # approach 1 - flatten and then binary search
-        nums = []
-        for i in matrix:
-            for j in i:
-                # print(j)
-                nums.append(j)
-        return self.binarySearch(nums, target)
+        row = len(matrix)
+        col = len(matrix[0])
+        l = [0,0]
+        r = [row - 1, col - 1]
+        while(l[0]*col + l[1] <= r[0]*col + r[1]):
+            sol = int((l[0]*col + l[1] + r[0]*col + r[1])/2)
+            mid = [int(sol/col), sol%col]
+            if matrix[mid[0]][mid[1]] == target:
+                return True
+            elif matrix[mid[0]][mid[1]] < target:
+                if mid[1] < col - 1:
+                    l = [mid[0], mid[1] + 1]
+                else:
+                    l = [mid[0]+1, 0]
+            else:
+                if mid[1]>0:
+                    r = [mid[0],mid[1] - 1]
+                else:
+                    r = [mid[0]-1, col - 1]
+        return False        
+    
+    
+    
+#    # approach 1 - flatten and then binary search
+#    def binarySearch(self, nums, target):
+#         l = 0
+#         r = len(nums) - 1
+#         while(l<=r):
+#             mid = int((l+r) / 2)
+#             if nums[mid] == target:
+#                 return True
+#             elif nums[mid] > target:
+#                 r = mid - 1
+#             else:
+#                 l = mid + 1
+#         return False
+    
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         nums = []
+#         for i in matrix:
+#             for j in i:
+#                 # print(j)
+#                 nums.append(j)
+#         return self.binarySearch(nums, target)
