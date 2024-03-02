fa5d2353f88c6eebf82c978359d77cb04fd8d532
shaurya singh <shauryasingh96@gmail.com>
2024-03-03 04:31:53 +0530

    Time: 37 ms (43.38%) | Memory: 16.4 MB (93.05%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,17 +1,30 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        self.memo = {}
-        a = self.solve(n)
-        print(self.memo)
-        return a
-    
-    def solve(self, n):
-        if n in self.memo.keys():
-            return self.memo[n]
         if n == 1:
             return 1
         elif n == 2:
             return 2
-        else:
-            self.memo[n] = self.solve(n-1) + self.solve(n-2)
-            return self.memo[n]
+        dp = [1,2]
+        for i in range (2,n):
+            dp.append(dp[i-1] + dp[i-2])
+        # print(dp)
+        return dp[n-1]
+
+# approach I
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         self.memo = {}
+#         a = self.solve(n)
+#         print(self.memo)
+#         return a
+    
+#     def solve(self, n):
+#         if n in self.memo.keys():
+#             return self.memo[n]
+#         if n == 1:
+#             return 1
+#         elif n == 2:
+#             return 2
+#         else:
+#             self.memo[n] = self.solve(n-1) + self.solve(n-2)
+#             return self.memo[n]
