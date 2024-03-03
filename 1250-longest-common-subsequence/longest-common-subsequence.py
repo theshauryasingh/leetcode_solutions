3f9812fe9c16cb754a5f45e6378a8e18e6f4e1ba
shaurya singh <shauryasingh96@gmail.com>
2024-03-04 01:06:30 +0530

    Time: 600 ms (34.93%) | Memory: 51.8 MB (20.80%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -8,8 +8,26 @@
 # "bsbininm"
 # "jmjkbkjkv"
 
-
-# Attempt III - bottom up
+# Attempt IV - bottom up - recursion - failed
+# class Solution:
+#     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+#         self.n1 = len(text1)
+#         self.n2 = len(text2)
+#         self.dp = [[-1]*(self.n1+1) for i in range(self.n2+1)] # [[0]*(n1+1)]*(n2+1)
+#         return self.solve(text1, text2, 0, 0)
+        
+#     def solve(self, text1, text2, i, j):
+#         if i >= self.n2 or j >= self.n1:
+#             return 0
+#         elif i == 0 or j ==0:
+#             self.dp[i][j] = 0
+#         elif text2[i-1] == text1[j-1]:
+#             self.dp[i][j] = 1 + self.dp[i - 1][j - 1]
+#         else:
+#             self.dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+#         return self.dp[i][j]
+            
+# # Attempt III - bottom up
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n1 = len(text1)
@@ -23,32 +41,6 @@ class Solution:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
         print(dp)
         return dp[n2][n1]
-    
-#      b  s  b  i  n  i  n  m
-# [[0, 1, 1, 2, 2, 2, 2, 2, 2],
-# j[0, 1, 1, 2, 2, 2, 2, 2, 2], 
-# m[0, 1, 1, 2, 2, 2, 2, 2, 2], 
-# j[0, 1, 1, 2, 2, 2, 2, 2, 2], 
-# k[0, 1, 1, 2, 2, 2, 2, 2, 2], 
-# b[0, 1, 1, 2, 2, 2, 2, 2, 2], 
-# k[0, 1, 1, 2, 2, 2, 2, 2, 2], 
-# j[0, 1, 1, 2, 2, 2, 2, 2, 2], 
-# k[0, 1, 1, 2, 2, 2, 2, 2, 2], 
-# v[0, 1, 1, 2, 2, 2, 2, 2, 2]] 
-# 9 8
-
-# 2 8
-# [[0, 0, 0, 0, 0, 0, 0, 0, 1], 
-#  [0, 0, 0, 0, 0, 0, 0, 0, 1], 
-#  [0, 0, 0, 0, 0, 0, 0, 0, 1], 
-#  [0, 0, 0, 0, 0, 0, 0, 0, 1], 
-#  [0, 0, 0, 0, 0, 0, 0, 0, 1], 
-#  [0, 0, 0, 0, 0, 0, 0, 0, 1], 
-#  [0, 0, 0, 0, 0, 0, 0, 0, 1], 
-#  [0, 0, 0, 0, 0, 0, 0, 0, 1], 
-#  [0, 0, 0, 0, 0, 0, 0, 0, 1], 
-#  [0, 0, 0, 0, 0, 0, 0, 0, 1]] 
-
 
 # # Attempt II - recursion  - memo
 # class Solution:
