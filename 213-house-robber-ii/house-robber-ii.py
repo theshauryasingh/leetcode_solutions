ddbe4ebf1a183c689156c0ef603e172f9bdd9a26
shaurya singh <shauryasingh96@gmail.com>
2024-03-03 09:23:15 +0530

    Time: 38 ms (47.37%) | Memory: 16.6 MB (52.08%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -16,34 +16,42 @@
 #             b = temp
 #         return b
 
-# # attempt III : dp array bottom up
-# class Solution:
-#     def rob(self, nums: List[int]) -> int:
-#         n = len(nums)
-#         dp = [0,nums[0]]
-#         for i in range(2,n+1):
-#             dp.append(max(dp[i-1], dp[i-2] + nums[i-1]))
-#         print(dp)
-#         return dp[n]
-
-
-# attempt II : recurrsion + memo 
+# attempt III : dp array bottom up
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
+        n = len(nums)
+        if n == 1:
             return nums[0]
-        self.memo = {}
-        a = self.solve(nums[:-1], 0)
-        self.memo = {}
-        b = self.solve(nums, 1)
-        return max(a, b)
+        dp = [0, nums[0]]
+        for i in range(2,n):
+            dp.append(max(dp[i-1], dp[i-2] + nums[i-1]))
+        print(dp)
+        a = max(dp[n-1], dp[n-2])
+        dp = [0, nums[1]]
+        for i in range(2,n):
+            dp.append(max(dp[i-1], dp[i-2] + nums[i]))
+        print(dp)
+        b = max(dp[n-1], dp[n-2])
+        return max(a,b)
+
+
+# # attempt II : recurrsion + memo 
+# class Solution:
+#     def rob(self, nums: List[int]) -> int:
+#         if len(nums) == 1:
+#             return nums[0]
+#         self.memo = {}
+#         a = self.solve(nums[:-1], 0)
+#         self.memo = {}
+#         b = self.solve(nums, 1)
+#         return max(a, b)
     
-    def solve(self, nums, i):
-        if i >= len(nums):
-            return 0
-        if i not in self.memo.keys():
-            self.memo[i] = max(nums[i] + self.solve(nums, i+2), self.solve(nums, i+1))
-        return self.memo[i]
+#     def solve(self, nums, i):
+#         if i >= len(nums):
+#             return 0
+#         if i not in self.memo.keys():
+#             self.memo[i] = max(nums[i] + self.solve(nums, i+2), self.solve(nums, i+1))
+#         return self.memo[i]
 
 # attempt I : failed - recurrsion + memo 
 # class Solution:
